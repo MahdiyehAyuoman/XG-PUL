bd052fa3c8b798f968266d32a9a906f0c6120f27
Mahdiyeh Ayooman <92025109+MahdiyehAyuoman@users.noreply.github.com>
2026-07-27 17:38:55 +0330

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+
+
+
+# ==========================
+# Project Paths
+# ==========================
+
+PATH_TO_DATASETS = "Datasets/"
+PATH_TO_RANKINGS = "Rankings/"
+PATH_TO_RESULTS = "Results/"
+PATH_TO_FIGURES = "Results/Figures/"
